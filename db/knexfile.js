a3d588527bfd6943de3946494730288fe3ce553a
Sujit Hona <suzit.hona@gmail.com>
2024-05-19 18:06:53 +0545

    first commit

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,13 +1,14 @@
 const path = require('path');
+require('dotenv').config();
 
 module.exports = {
   development: {
     client: 'mysql',
     connection: {
-      host: 'localhost',
-      user: 'hona',
-      password: '',
-      database: 'back'
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     },
     migrations: {
       tableName: 'knex_migrations',
